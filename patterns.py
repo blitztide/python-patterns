b13b3e58b538022af95a5abe56618fe9ccd772f1
blitztide <david.elliott3040@gmail.com>
2021-01-12 11:20:04 +0000

    Added comments, bugfixing

--- patterns.py
+++ patterns.py
@@ -6,6 +6,7 @@ Date: 10/01/2021
 Version: 1
 Description: Generate/search sequential sequence for use in BOF.
 The pattern is a block of 3 characters  A B and C where:
+[ABC][ABC][ABC]
     A -> A-Z
     B -> 0-9
     C -> a-z
@@ -27,8 +28,8 @@ def convert_to_hex(string):
         #Is 0x12312312312 format
         # Split string into 2 nibble chunks
         hexarray = b""
-        for x in range(2,len(string),2):
-            hexarray += chr(int(string[x]+string[x+1],16))
+        for x in range(2,len(string),2):  #Select every other byte
+            hexarray += chr(int(string[x]+string[x+1],16)) # convert two bytes into actual hex character then append to hex array
         return hexarray #Return byte array
     return string
 
@@ -36,8 +37,8 @@ def create(size):
     charcode=41
     buffer = ""
     for x in range(size):
-        position = chr((x % 3)+65)  #Selects position within argument ("Makes it easier to understand")
-        block = x//3 #Floor of x/3
+        position = chr((x % 3)+65)  #Selects position within argument, 0 -> A 1 -> B 2 -> C
+        block = x//3 #Returns block number of x
         if position == "A": #Increment every 260th block bound to A-Z
             buffer += chr((block//260 % 26)+65)
         if position == "B": #Increment Every 26th block bound to 0-9
@@ -52,46 +53,47 @@ def offset(chars):
     # Search for capital letter in EIP, determine largest offset size
     for x in chars:
         if x in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-            largestsize=(ord(x)-64)*260*3 #Finds Largest Capital Letter
-        buffer = create(largestsize) #Creates the searchspace
-    for y in range(0,largestsize):
+            lettersize=(ord(x)-64)*260*3 #Finds current capital letter size
+            if lettersize > largestsize: #If current size is larger than stored largest size, set largest size to letter size.
+                largestsize = lettersize
+        buffer = create(largestsize) #Creates the smallest pattern where search string could reside.
+    for y in range(0,largestsize):   #Sliding 4 byte search using little endian
         if buffer[y] == chars[3]:
             if buffer[y+1] == chars[2]:
                 if buffer[y+2] == chars[1]:
                     if buffer[y+3] == chars[0]:
-                        return int(y)
+                        return int(y) #Only return when all 4 bytes match
                     continue
                 continue
             continue
         continue
 
-def badchars(size,chars):
-    buffer = b"\x41" * (int(size)-256-20)
+def badchars(size,chars):   # Generate a buffer with all possible characters, then append with 0x41 characters
     for x in range(0,256):
-        if not chr(x) in convert_to_hex(args.b):
+        if not chr(x) in convert_to_hex(args.b): # Ignore all badchars specified in badchars argument
             buffer += chr(x)
-    buffer += "D" * 20 #Easily see end of buffer in debugger.
+    buffer += b"\x41" * (int(size)-len(buffer))
     return buffer
 
-def exploit(size,nops,shellcode,EIP,offset):
-    nopsled = b"\x90" * int(nops)
-    eip_val = bytearray(EIP)
-    buffer = eip_val + nopsled + shellcode
-    post_padding_size = size - offset - int(nops) - len(shellcode) - 4   #Finds the amound of post-padding required
+def exploit(size,nops,shellcode,EIP,offset): # Create a buffer which is: [PADDING][EIP][NOPSLED][SHELLCODE][PADDING]
+    nopsled = b"\x90" * int(nops)   # Create nopsled to desired size
+    eip_val = bytearray(EIP) # Get EIP value
+    buffer = eip_val + nopsled + shellcode # Create Exploit buffer
+    post_padding_size = size - offset - int(nops) - len(shellcode) - 4   #Finds the amount of post-padding required
     buffer = "A" * offset + buffer
     buffer += "A" * post_padding_size
     return buffer
 
 
-def main():
+def main():    # Mode selection routine.
     if (args.mode == "C"):
         if(args.v):
-            print ("Creating pattern of size: " + args.positional)
+            print("Creating pattern of size: " + args.positional)
         print(create(int(args.positional)))
     if (args.mode == "O"):
         if args.v:
             print("Finding offset of: " + args.positional)
-        print(offset(args.positional))
+        print(offset(convert_to_hex(args.positional)))
     if (args.mode == "B"):
         if args.v:
             print("Generating Bad Pattern of size: " + args.positional)
@@ -100,9 +102,6 @@ def main():
         shellcode = sys.stdin.read()
         sys.stdout.write(exploit(int(args.positional),int(args.n),shellcode,convert_to_hex(args.e),args.o))
         
-
-   
-
 
 
 if __name__ == "__main__":
